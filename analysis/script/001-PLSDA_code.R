e16b47f1b2ecf46e9b6b19d22719142bec363c21
Florencia Marcaccio <fmarca@uw.edu>
2020-02-23 21:51:20 -0800

    Adding some of the changes in our code

--- analysis/script/001-PLSDA_code.R
+++ analysis/script/001-PLSDA_code.R
@@ -1,11 +1,22 @@
 library(MetaboAnalystR)
-source('functions.r')
+
+
+uninformative_filter <- function(input, method) {
+  valid_vals <- c("iqr", "mean", "median", "nrsd", "rsd", "mad", "sd")
+  if (!(method %in% valid_vals)) {
+    stop('Incorrect filtering method specified')
+  }
+  output <- FilterVariable(input, method, "F", 25)
+  output
+}
+
+
 mSet<-InitDataObjects("specbin", "stat", FALSE)
 mSet<-Read.TextData(mSet, "script/data_processed.csv", "rowu", "disc");
 mSet<-SanityCheckData(mSet)
 mSet<-RemoveMissingPercent(mSet, percent=0.5)
-mSet<-ImputeVar(mSet, method="min")
-mSet<-FilterVariable(mSet, "iqr", "F", 25)
+mSet<-ImputeVar(mSet, method="mean")
+mSet<-uninformative_filter(mSet, "sd")
 mSet<-PreparePrenormData(mSet)
 mSet<-Normalization(mSet, "NULL", "NULL", "NULL", ratio=FALSE, ratioNum=20)
 mSet<-PLSR.Anal(mSet, reg=TRUE)
