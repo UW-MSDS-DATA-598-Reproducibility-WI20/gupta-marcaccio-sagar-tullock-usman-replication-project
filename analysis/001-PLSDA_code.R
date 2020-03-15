ef3c9613c0db67e6073a90cf3c91efb4ca64c3c1
Ben Marwick <benmarwick@hotmail.com>
2020-03-15 13:49:30 -0700

    Update 001-PLSDA_code.R

--- analysis/001-PLSDA_code.R
+++ analysis/001-PLSDA_code.R
@@ -1,4 +1,7 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
 
+BiocManager::install("CAMERA")
 
 if (!require("MetaboAnalystR")) pacman::p_load(Rserve, RSclient, ellipse, scatterplot3d, Cairo, randomForest, caTools, e1071, som, impute, pcaMethods, RJSONIO, ROCR, globaltest, GlobalAncova, Rgraphviz, preprocessCore, genefilter, pheatmap, SSPA, sva, Rcpp, pROC, data.table, limma, car, fitdistrplus, lars, Hmisc, magrittr, methods, xtable, pls, caret, lattice, igraph, gplots, KEGGgraph, reshape, RColorBrewer, tibble, siggenes, plotly, fgsea, metap, reshape2, scales, CAMERA); devtools::install_github("xia-lab/MetaboAnalystR")
 
